19f002ef1c775a5ba93b565063be8ab604e21dbd
Irene Rodriguez Lujan <irene@ArdiMarmos-mc.local>
2024-01-21 12:58:58 +0100

    first rouund

---
+++ notebooks/plot_decision_boundary.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_decision_boundary(clf, X, Y, cmap='Paired_r'):
+    h = 0.02
+    x_min, x_max = X[:,0].min() - 10*h, X[:,0].max() + 10*h
+    y_min, y_max = X[:,1].min() - 10*h, X[:,1].max() + 10*h
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    plt.figure(figsize=(5,5))
+    plt.contourf(xx, yy, Z, cmap=cmap, alpha=0.25)
+    plt.contour(xx, yy, Z, colors='k', linewidths=0.7)
+    plt.scatter(X[:,0], X[:,1], c=Y, cmap=cmap, edgecolors='k');
+
