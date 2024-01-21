d3409ecaf7efdaf6a47695b90ba9ac7eacd11069
Irene Rodriguez Lujan <irene@ArdiMarmos-mc.local>
2024-01-21 14:37:33 +0100

    notebooks update

--- notebooks/plot_decision_boundary.py
+++ notebooks/plot_decision_boundary.py
@@ -1,10 +1,19 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 def plot_decision_boundary(clf, X, Y, cmap='Paired_r'):
     h = 0.02
-    x_min, x_max = X[:,0].min() - 10*h, X[:,0].max() + 10*h
-    y_min, y_max = X[:,1].min() - 10*h, X[:,1].max() + 10*h
+
+    if isinstance(X,np.ndarray):
+        x_min, x_max = X[:,0].min() - 10*h, X[:,0].max() + 10*h
+        y_min, y_max = X[:,1].min() - 10*h, X[:,1].max() + 10*h
+    elif isinstance(X,pd.DataFrame):
+        x_min, x_max = X.iloc[:,0].min() - 10*h, X.iloc[:,0].max() + 10*h
+        y_min, y_max = X.iloc[:,1].min() - 10*h, X.iloc[:,1].max() + 10*h
+    else:
+        raise TypeError("incorrect type for X parameter")
+        
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                          np.arange(y_min, y_max, h))
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
@@ -13,5 +22,8 @@ def plot_decision_boundary(clf, X, Y, cmap='Paired_r'):
     plt.figure(figsize=(5,5))
     plt.contourf(xx, yy, Z, cmap=cmap, alpha=0.25)
     plt.contour(xx, yy, Z, colors='k', linewidths=0.7)
-    plt.scatter(X[:,0], X[:,1], c=Y, cmap=cmap, edgecolors='k');
+    if isinstance(X,np.ndarray): 
+        plt.scatter(X[:,0], X[:,1], c=Y, cmap=cmap, edgecolors='k');
+    else:
+        plt.scatter(X.iloc[:,0], X.iloc[:,1], c=Y, cmap=cmap, edgecolors='k');
 
